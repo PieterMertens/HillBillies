9187740b82c88c6bc9b991c96bde04fcbca97b37
Pieter <pieter_mertens13@hotmail.com>
2016-02-29 22:18:16 +0100

    name toegevoegd aan unit

--- src/hillbillies/model/Unit.java
+++ src/hillbillies/model/Unit.java
@@ -69,6 +69,80 @@ public void setPosition(Position position)
 /**
  * Variable registering the position of this unit.
  */
-private property_type position;
+private Position position;
+
+
+
+/** TO BE ADDED TO CLASS HEADING
+ * @invar  The name of each unit must be a valid name for any
+ *         unit.
+ *       | isValidName(getName())
+ */
+
+
+/**
+ * Initialize this new unit with given name.
+ *
+ * @param  name
+ *         The name for this new unit.
+ * @effect The name of this new unit is set to
+ *         the given name.
+ *       | this.setName(name)
+ */
+public Unit(String name)
+		throws IllegalArgumentException {
+	this.setName(name);
+}
+
+
+/**
+ * Return the name of this unit.
+ */
+@Basic @Raw
+public String getName() {
+	return this.name;
+	
+	
+}
+
+/**
+ * Check whether the given name is a valid name for
+ * any unit.
+ *  
+ * @param  name
+ *         The name to check.
+ * @return 
+ *       | result == 
+*/
+public static boolean isValidName(String name) {
+	return false;
+}
+
+/**
+ * Set the name of this unit to the given name.
+ * 
+ * @param  name
+ *         The new name for this unit.
+ * @post   The name of this new unit is equal to
+ *         the given name.
+ *       | new.getName() == name
+ * @throws IllegalArgumentException
+ *         The given name is not a valid name for any
+ *         unit.
+ *       | ! isValidName(getName())
+ */
+@Raw
+public void setName(String name) 
+		throws IllegalArgumentException {
+	if (! isValidName(name))
+		throw new IllegalArgumentException();
+	this.name = name;
+}
+
+/**
+ * Variable registering the name of this unit.
+ */
+private String name;
+
 
 }
